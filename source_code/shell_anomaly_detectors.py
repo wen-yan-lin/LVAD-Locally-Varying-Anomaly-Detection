fa267e581851d8b433ae1056c04270face553e2a
wen-yan-lin <linwenyan.daniel1@gmail.com>
2022-07-22 21:38:10 +0800

    cat

--- source_code/shell_anomaly_detectors.py
+++ source_code/shell_anomaly_detectors.py
@@ -1,12 +1,25 @@
+""" <Local Varying Anomaly Detection>
+    Copyright (C) <2022>  <Wen-Yan Lin>
+    daniellin@smu.edu.sg
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+
 import timeit
 import numpy as np
 from sklearn.metrics import pairwise_distances
 from sklearn.cluster import KMeans
 import tensorflow as tf
-#from dagmm import DAGMM
-#from DeepUnsupAD import DistributionCluster
-#from DeepUnsupAD.de2e import DE2E
-
 
 def est_cummilative_dist(distance, multiples = 10):
     """ estimate the threshold for each percentile"""
